5fdd9cbea3a0f3863b2812800944ee4186366422
markporo <markporo@yahoo.com>
2021-07-25 06:02:42 -0400

    met mvp requirements...prop drilled correctly

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import Post from './Post';
+//import PostHeader from './PostHeader';
 import './Posts.css';
 
-const Posts = (props) => {
+const Posts = ({ likePost, posts }) => {
   // 🔥 Make sure the parent of Posts is passing the right props!
-  const { likePost, posts } = props;
+  // const  { likePost, posts } = props;
 
   return (
     <div className='posts-container-wrapper'>
       {/* Map through the posts array returning a Post component at each iteration */}
+      {posts.map(eachPost => {
+        return <Post post={eachPost} likePost={likePost} />
+      })}
       {/* Check the implementation of Post to see what props it requires! */}
     </div>
   );
